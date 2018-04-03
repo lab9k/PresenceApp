e8fe8580f3903da67b88d7e7c04b38389c7a3942
Wannes Vereecken <wannes.vereecken@hotmail.be>
2018-04-03 12:05:33 +0200

    heroku

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ exports.creds = {
     responseMode: 'form_post', 
   
     // Required, the reply URL registered in AAD for your app
-    redirectUrl: 'https://agile-everglades-38755.herokuapp.com/auth/openid/return', 
+    redirectUrl: 'https://samloffice365digipolisgent.gent.be/saml/idp/profile/redirectorpost/sso', 
     //redirectUrl: 'http://localhost:5000/auth/openid/return',
 
     // Required if we use http for redirectUrl
