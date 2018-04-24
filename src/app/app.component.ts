39abf959bd5bf168690852910a29ccdc8706fe12
Wannes Vereecken <wannes.vereecken@hotmail.be>
2018-04-24 15:09:34 +0200

    styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { AuthenticationService } from './shared/services/authentication.service';
+import { Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 
 declare var $: any;
 
@@ -9,12 +11,18 @@ declare var $: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
 
   private _currentUser: string;
   private _correctIp: boolean;
+  private _campusList: any;
 
-  constructor(private authService: AuthenticationService) {
+  constructor(private authService: AuthenticationService, private router: Router) {
+  }
+
+  ngOnInit() {
+    $('.ui.dropdown')
+      .dropdown();
   }
 
   get currentUser(): Observable<string> {
@@ -26,7 +34,27 @@ export class AppComponent  {
     .dropdown();
   }
 
+  onChange(theme) {
+    console.log(theme);
+    switch (theme) {
+      case 'default':
+        $('#theme').attr('href', 'assets/semantic.min.css');
+        break;
+      default:
+        $('#theme').attr('href', 'assets/semantic.' + theme + '.min.css');
+        break;
+    }
+  }
+
   get correctIp() {
     return this._correctIp;
+  }
+
+  onActivate(componentRef) {
+    this._campusList = componentRef;
+  }
+
+  filter(event) {
+    this._campusList.filter(event);
   }
 }
